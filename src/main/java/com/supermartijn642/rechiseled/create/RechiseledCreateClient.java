1ace1bdbbfce11bd7adb1c3414db13d3b0dbc17e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-10-17 21:56:54 +0200

    Fix crash with newer core lib versions #6

--- src/main/java/com/supermartijn642/rechiseled/create/RechiseledCreateClient.java
+++ src/main/java/com/supermartijn642/rechiseled/create/RechiseledCreateClient.java
@@ -4,7 +4,6 @@ import com.jozufozu.flywheel.backend.instancing.InstancedRenderRegistry;
 import com.simibubi.create.foundation.item.KineticStats;
 import com.simibubi.create.foundation.item.TooltipModifier;
 import com.supermartijn642.core.registry.ClientRegistrationHandler;
-import com.supermartijn642.core.registry.RegistrationHandler;
 import com.supermartijn642.core.render.CustomRendererBakedModelWrapper;
 import com.supermartijn642.rechiseled.create.mechanical_chisel.MechanicalChiselItemRenderer;
 import com.supermartijn642.rechiseled.create.mechanical_chisel.MechanicalChiselRenderInstance;
@@ -21,13 +20,9 @@ public class RechiseledCreateClient implements ClientModInitializer {
         // Register mechanical chisel renderer
         ClientRegistrationHandler handler = ClientRegistrationHandler.get(RechiseledCreate.MODID);
         handler.registerBlockEntityRenderer(() -> RechiseledCreate.mechanical_chisel_entity, MechanicalChiselRenderer::new);
-        RegistrationHandler.get(RechiseledCreate.MODID).registerBlockEntityTypeCallback(helper ->
-            InstancedRenderRegistry.configure(RechiseledCreate.mechanical_chisel_entity).factory(MechanicalChiselRenderInstance::new).apply()
-        );
+        InstancedRenderRegistry.configure(RechiseledCreate.mechanical_chisel_entity).factory(MechanicalChiselRenderInstance::new).apply();
         handler.registerCustomItemRenderer(() -> RechiseledCreate.mechanical_chisel.asItem(), MechanicalChiselItemRenderer::new);
         handler.registerItemModelOverwrite(() -> RechiseledCreate.mechanical_chisel.asItem(), CustomRendererBakedModelWrapper::wrap);
-        RegistrationHandler.get(RechiseledCreate.MODID).registerItemCallback(helper ->
-            TooltipModifier.REGISTRY.register(RechiseledCreate.mechanical_chisel.asItem(), KineticStats.create(RechiseledCreate.mechanical_chisel.asItem()))
-        );
+        TooltipModifier.REGISTRY.register(RechiseledCreate.mechanical_chisel.asItem(), KineticStats.create(RechiseledCreate.mechanical_chisel.asItem()));
     }
 }
