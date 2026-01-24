ca3225ba186e12aaf8f652903bb4521b7227bdfb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-24 10:39:41 +0100

    Update to Rechiseled 1.2.0

---
+++ src/main/java/com/supermartijn642/rechiseled/create/compat/rei/RechiseledCreateREIPlugin.java
@@ -0,0 +1,20 @@
+package com.supermartijn642.rechiseled.create.compat.rei;
+
+import com.supermartijn642.rechiseled.compat.rei.ChiselingREIPlugin;
+import com.supermartijn642.rechiseled.create.RechiseledCreate;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import me.shedaniel.rei.forge.REIPluginClient;
+
+/**
+ * Created 29/07/2025 by SuperMartijn642
+ */
+@REIPluginClient
+public class RechiseledCreateREIPlugin implements REIClientPlugin {
+
+    @Override
+    public void registerCategories(CategoryRegistry registry){
+        registry.addWorkstations(ChiselingREIPlugin.CHISELING_CATEGORY, EntryStacks.of(RechiseledCreate.mechanical_chisel));
+    }
+}
