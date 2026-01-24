10690efdcaaa399aeef995f44d43fb323117e9c7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-24 10:39:41 +0100

    Update to Rechiseled 1.2.0

---
+++ src/main/java/com/supermartijn642/rechiseled/create/compat/jei/RechiseledCreateJEIPlugin.java
@@ -0,0 +1,25 @@
+package com.supermartijn642.rechiseled.create.compat.jei;
+
+import com.supermartijn642.rechiseled.compat.jei.ChiselingJEIPlugin;
+import com.supermartijn642.rechiseled.create.RechiseledCreate;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import net.minecraft.resources.ResourceLocation;
+
+/**
+ * Created 28/12/2021 by SuperMartijn642
+ */
+@JeiPlugin
+public class RechiseledCreateJEIPlugin implements IModPlugin {
+
+    @Override
+    public ResourceLocation getPluginUid(){
+        return ResourceLocation.fromNamespaceAndPath(RechiseledCreate.MODID, "chiseling_plugin");
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration){
+        registration.addRecipeCatalyst(RechiseledCreate.mechanical_chisel, ChiselingJEIPlugin.CHISELING_RECIPE_TYPE);
+    }
+}
